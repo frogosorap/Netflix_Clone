78ac536a8e9474469ac8d867036af3fe8e156633
Harjappan Singh <singhharjappan@gmail.com>
2024-04-16 17:06:12 +0100

    created movies controller and added auth routes, views and controller

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
+use App\Http\Controllers\MoviesController;
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/home', function () {
-    return view('homepage');
-});
 
 Route::get('/index', [PagesController::class, "index"]);
+
+Route::resource('/movie', MoviesController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
