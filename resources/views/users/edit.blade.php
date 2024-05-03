cf492b63ff758be507427dfbc1ac3bedb1ef54f2
Raphael Frogoso <alkali.smiley@gmail.com>
2024-05-03 11:09:29 +0100

    Formatting the CRUD for admin users dashboard

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,61 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>Edit User Form - Laravel 8 CRUD Tutorial</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+    <meta charset="UTF-8">
+    <title>Edit User Form - Laravel 8 CRUD Tutorial</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    @vite('resources/css/app.css')
 </head>
 <body>
-<div class="container mt-2">
-<div class="row">
-<div class="col-lg-12 margin-tb">
-<div class="pull-left">
-<h2>Edit User</h2>
-</div>
-<div class="pull-right">
-<a class="btn btn-primary" href="{{ route('users.index') }}" enctype="multipart/form-data"> Back</a>
-</div>
-</div>
-</div>
-@if(session('status'))
-<div class="alert alert-success mb-1 mt-1">
-{{ session('status') }}
-</div>
-@endif
-<form action="{{ route('users.update',$user->id) }}" method="POST" enctype="multipart/form-data">
-@csrf
-@method('PUT')
-<div class="row">
-<div class="col-xs-12 col-sm-12 col-md-12">
-<div class="form-group">
-<strong>User Name:</strong>
-<input type="text" name="name" value="{{ $user->name }}" class="form-control" placeholder="User name">
-@error('name')
-<div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-@enderror
-</div>
-</div>
-<div class="col-xs-12 col-sm-12 col-md-12">
-<div class="form-group">
-<strong>User Email:</strong>
-<input type="email" name="email" class="form-control" placeholder="User Email" value="{{ $user->email }}">
-@error('email')
-<div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-@enderror
-</div>
-</div>
-<div class="col-xs-12 col-sm-12 col-md-12">
-<div class="form-group">
-<strong>User Password:</strong>
-<input type="text" name="password" value="{{ $user->password }}" class="form-control" placeholder="User Password">
-@error('password')
-<div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-@enderror
-</div>
-</div>
-<button type="submit" class="btn btn-primary ml-3">Submit</button>
-</div>
-</form>
-</div>
+    <div class="crud-container">
+        <div class="row">
+            <div class="col-lg-12 crud-margin-tb">
+                <div class="pull-left">
+                    <h2>Edit User</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('users.index') }}"> Back</a>
+                </div>
+            </div>
+        </div>
+        @if(session('status'))
+            <div class="crud-alert crud-alert-success crud-mb-1 crud-mt-1">
+                {{ session('status') }}
+            </div>
+        @endif
+        <form action="{{ route('users.update',$user->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>User Name:</strong>
+                        <input type="text" name="name" value="{{ $user->name }}" class="form-control" placeholder="User name">
+                        @error('name')
+                            <div class="crud-alert crud-alert-danger crud-mt-1 crud-mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>User Email:</strong>
+                        <input type="email" name="email" class="form-control" placeholder="User Email" value="{{ $user->email }}">
+                        @error('email')
+                            <div class="crud-alert crud-alert-danger crud-mt-1 crud-mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>User Password:</strong>
+                        <input type="text" name="password" value="{{ $user->password }}" class="form-control" placeholder="User Password">
+                        @error('password')
+                            <div class="crud-alert crud-alert-danger crud-mt-1 crud-mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary ml-3">Submit</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,52 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>Laravel 8 CRUD Tutorial From Scratch</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+    <meta charset="UTF-8">
+    <title>Laravel 8 CRUD Tutorial From Scratch</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    @vite('resources/css/app.css')
+
 </head>
 <body>
-<div class="container mt-2">
-<div class="row">
-<div class="col-lg-12 margin-tb">
-<div class="pull-left">
-<h2>Laravel 8 CRUD Example Tutorial</h2>
-</div>
-<div class="pull-right mb-2">
-<a class="btn btn-success" href="{{ route('users.create') }}"> Create User</a>
-</div>
-</div>
-</div>
-@if ($message = Session::get('success'))
-<div class="alert alert-success">
-<p>{{ $message }}</p>
-</div>
-@endif
-<table class="table table-bordered">
-<tr>
-<th>S.No</th>
-<th>User Name</th>
-<th>User Email</th>
-<th>User Password</th>
-<th width="280px">Action</th>
-</tr>
-@foreach ($users as $user)
-<tr>
-<td>{{ $user->id }}</td>
-<td>{{ $user->name }}</td>
-<td>{{ $user->email }}</td>
-<td>{{ $user->password }}</td>
-<td>
-<form action="{{ route('users.destroy',$user->id) }}" method="Post">
-<a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
-@csrf
-@method('DELETE')
-<button type="submit" class="btn btn-danger">Delete</button>
-</form>
-</td>
-</tr>
-@endforeach
-</table>
-{!! $users->links() !!}
+    <div class="crud-container">
+        <div class="row">
+            <div class="col-lg-12 crud-margin-tb">
+                <div class="pull-left">
+                    <h2>Laravel 8 CRUD Example Tutorial</h2>
+                </div>
+                <div class="pull-right crud-mb-2">
+                    <a class="btn btn-success" href="{{ route('users.create') }}"> Create User</a>
+                </div>
+            </div>
+        </div>
+        @if ($message = Session::get('success'))
+            <div class="crud-alert crud-alert-success">
+                <p>{{ $message }}</p>
+            </div>
+        @endif
+        <table class="table table-bordered">
+            <tr>
+                <th>S.No</th>
+                <th>User Name</th>
+                <th>User Email</th>
+                <th>User Password</th>
+                <th width="280px">Action</th>
+            </tr>
+            @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->password }}</td>
+                    <td>
+                        <form action="{{ route('users.destroy',$user->id) }}" method="Post">
+                            <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+        {!! $users->links() !!}
+    </div>
 </body>
 </html>
